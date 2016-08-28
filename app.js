f5f10685c4fe5230641dfa90d3f20f4e43c7ccd6
ashleyvernon <avernon526@gmail.com>
2016-08-28 10:33:25 -0700

    keys with animate to move object

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
+$(function() {
+	var player = '<div id="planet1"></div>';
+	var player2 ='div id="planet2"></div>';
+	$("#spacetrack").append(player);
 
-$( document ).ajaxStart(function() {
-  $( ".log" ).text( "Triggered ajaxStart handler." );
+
+
+})
+
+$(document).ready(function() {
+	$(document).keydown(function(key) {
+		switch(parseInt(key.which,10)) {
+			// Left arrow key pressed
+			case 37:
+				$('#planet1').animate({left: "-=50px"}, 'fast');
+				break;
+			// Up Arrow Pressed
+			case 38:
+				$('#planet1').animate({top: "-=50px"}, 'fast');
+				break;
+			// Right Arrow Pressed
+			case 39:
+				$('#planet1').animate({left: "+=50px"}, 'fast');
+				break;
+			// Down Arrow Pressed
+			case 40:
+
+				$('#planet1').animate({top: "+=50px"}, 'fast');
+				break;
+		}
+
+	});
 });
 
-$( ".trigger" ).click(function() {
-  $( ".result" ).load( "ajax/test.html" );
-
-});
